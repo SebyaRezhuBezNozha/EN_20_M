99933264ebd9926028e55ef0a31b096da125a1c5
SebyaRezhuBezNozha <sebya-rezhu-bez-nozha-dostabl@yandex.ru>
2026-09-22 04:58:17 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -343,267 +343,172 @@ function load(){
 
 // EN songs
 
-const en_2020_gr_icon = [
-	'pop',
-	'rock'
+const en_2020_m_icon = [
+	'pop'
 ];
 
-const EN_2020_GR_PACK_1 = 1;
-const EN_2020_GR_PACK_2 = 2;
+const EN_2020_M_PACK_1 = 1;
 
-let en_2020_gr = [
+let en_2020_m = [
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Jonas Brothers',
-		song : "What a Man Gotta Do (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Troye Sivan',
+		song : "Rush (2023)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Jonas Brothers',
-		song : "Leave Before You Love Me (ft Marshmello) (2021)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Troye Sivan',
+		song : "One of Your Girls (2023)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Jonas Brothers',
-		song : "Waffle House (2023)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Troye Sivan',
+		song : "Got Me Started (2023)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Imagine Dragons',
-		song : "Blank Space (2022)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Oliver Tree',
+		song : "Jerk (2020)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Imagine Dragons',
-		song : "Bones (2022)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Oliver Tree',
+		song : "Life Goes On  (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Imagine Dragons',
-		song : "Enemy  (2021)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Oliver Tree',
+		song : "Let Me Down (2020)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Tame Impala',
-		song : "Is It True (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Benson Boone',
+		song : "Ghost Town (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Tame Impala',
-		song : "Lost in Yesterday (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Benson Boone',
+		song : "Beautiful Things (2024)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Tame Impala',
-		song : "Breathe Deeper (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Benson Boone',
+		song : "Slow It Down (2024)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Thirty Seconds to Mars',
-		song : "Lost These Days (2023)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Tom Odell',
+		song : "Black Friday (2023)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Thirty Seconds to Mars',
-		song : "Stuck (2023)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Tom Odell',
+		song : "over you yet (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Thirty Seconds to Mars',
-		song : "Seasons (2023)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Tom Odell',
+		song : "numb (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : '5 Seconds of Summer',
-		song : "Lonely Heart (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Ed Sheeran',
+		song : "Shivers (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'A Day to Remember',
-		song : "Mindreader (2020)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Ed Sheeran',
+		song : "Azizam (2025)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Asking Alexandria',
-		song : "Alone Again (2021)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Ed Sheeran',
+		song : "Eyes Closed (2023)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Asking Alexandria',
-		song : "Dark Void (2024)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Weeknd',
+		song : "Too Late (2020)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Bastille',
-		song : "Distorted Light Beam (2021)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Weeknd',
+		song : "Is There Someone Else? (2022)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Black Keys',
-		song : "Wild Child (2022)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Weeknd',
+		song : "Save Your Tears (2020)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Bring Me The Horizon',
-		song : "Kingslayer (ft BABYMETAL) (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Kendrick Lamar',
+		song : "Not Like Us (2024)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Cigarettes After Sex',
-		song : "Stop Waiting (2023)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Kendrick Lamar',
+		song : "Squabble Up (2024)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Coldplay',
-		song : "My Universe (ft BTS) (2021)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Kendrick Lamar',
+		song : "Luther (ft SZA) (2024)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Electric Callboy',
-		song : "Arrow of Love (2022)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Post Malone',
+		song : "Chemical (2023)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Electric Callboy',
-		song : "We Got the Moves (2021)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Post Malone',
+		song : "I Had Some Help (ft Morgan Wallen) (2024)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Enter Shikari',
-		song : "It Hurts (2023)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Post Malone',
+		song : "I Like You (A Happier Song) (ft Doja Cat) (2022)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Falling In Reverse',
-		song : "God Is a Weapon (ft Marilyn Manson) (2025)"
+		pack : EN_2020_M_PACK_1,
+		group : 'James Arthur',
+		song : "Car's Outside (2022)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Falling In Reverse',
-		song : "Watch the World Burn (2023)"
+		pack : EN_2020_M_PACK_1,
+		group : 'James Arthur',
+		song : "Lasting Lover (ft Sigala) (2020)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Green Day',
-		song : "Holy Toledo! (2021)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Lil Nas X',
+		song : "MONTERO (Call Me By Your Name) (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'KALEO',
-		song : "Hey Gringo (2021)",
-		ignore : true
+		pack : EN_2020_M_PACK_1,
+		group : 'Lil Nas X',
+		song : "THATS WHAT I WANT (2021)"
 	},
 	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Muse',
-		song : "Will Of The People (2022)",
-		ignore : true
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Placebo',
-		song : "Forever Chemicals (2022)",
-		ignore : true
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Sabaton',
-		song : "Defence of Moscow (2021)"
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Sabaton',
-		song : "The Attack Of The Dead Men (2020)"
-	},
-	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Score',
-		song : "Fighter (2022)"
-	},
-	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Score',
-		song : "Head Up (2021)"
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Slaughter to Prevail',
-		song : "Demolisher (2020)",
-		ignore : true
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Smash Into Pieces',
-		song : "Big Bang (2020)"
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Smash Into Pieces',
-		song : "Forever Alone (2020)"
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Three Days Grace',
-		song : "Mayday (2024)",
-		ignore : true
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Volbeat',
-		song : "Shotgun Blues (2021)"
-	},
-	{
-		pack : EN_2020_GR_PACK_2,
-		group : 'Lamb of God',
-		song : "Dead Seeds (2020)"
-	},
-	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'Imagine Dragons',
-		song : "Lonely (2021)",
-		ignore : true
-	},
-	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'BTS',
-		song : "Dynamite (2020)"
-	},
-	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'BTS',
-		song : "Butter (2021)"
-	},
-	{
-		pack : EN_2020_GR_PACK_1,
-		group : 'BTS',
-		song : "ON (2020)"
+		pack : EN_2020_M_PACK_1,
+		group : 'Lil Nas X',
+		song : "Industry Baby (ft Jack Harlow) (2021)"
 	}
 ];
 
-let en_2020_gr_1 = en_2020_gr.filter(item => item.pack == 1);
-let en_2020_gr_2 = en_2020_gr.filter(item => item.pack == 2);
+let en_2020_m_1 = en_2020_m.filter(item => item.pack == 1);
 
 let music = [
 	{
-		arr: en_2020_gr,
+		arr: en_2020_m,
 		lang: 'en',
 		year: '2020',
-		type: 'gr',
+		type: 'm',
 		packs: [
 				{
-					arr: en_2020_gr_2,
-					name: 'EN 2020s Groups: Rock',
-				},
-				{
-					arr: en_2020_gr_1,
-					name: 'EN 2020s Groups: Pop',
+					arr: en_2020_m_1,
+					name: 'EN 2020s Male: Pop'
 				}
 			]
 	}
@@ -933,13 +838,13 @@ let generateImgPath;
 function setup(){
 	lang = 'en';
 	year = '2020';
-	artist_type = 'gr';
+	artist_type = 'm';
 	back = back_to_packages;
 	modeToggle = toggleArtist;
 	setMedia = setAudio;
 	rightAnswer = rightAnswer_RU;
 	count_time();
-	package_names = en_2020_gr_icon;
+	package_names = en_2020_m_icon;
 	show_packages(package_names.length);
 	document.body.scrollTop = document.documentElement.scrollTop = 0;
 	useUrlParam();
